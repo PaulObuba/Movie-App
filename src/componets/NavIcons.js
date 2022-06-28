f4169cb0c5962f105d3f4d683d0d5bc4636b9e43
Paul Obuba <paulobuba26@gmail.com>
2022-06-28 16:38:32 +0100

    Navbar update

--- src/componets/NavIcons.js
+++ src/componets/NavIcons.js
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
-import NavIcons from "./NavIcons";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import React from "react";
 
-const Navbar = ({ setTerm, setUrl, term }) => {
-  const [navIcon, setNavIcon] = useState(false);
-
+const NavIcons = ({ setTerm, setUrl }) => {
   const popular = () => {
     setTerm("popular");
     setUrl(
@@ -24,37 +20,25 @@ const Navbar = ({ setTerm, setUrl, term }) => {
     );
   };
 
-  const handleChange = () => {
-    setNavIcon(!navIcon);
-  };
-
   return (
-    <div
-      className="flex justify-between align-center px-8 py-8 mb-3
-                            shadow-lg text-white font-bold font-mono"
-    >
-      <div className="px-3">
-        <p className="lg:text-3xl bold uppercase"> {term} </p>
-      </div>
-
-      <div className="lg:hidden w-7 text-white z-50" onClick={handleChange}>
-        {navIcon ? <XIcon /> : <MenuIcon />}
-      </div>
-
-      {/* large NavIcon */}
-      <div className="hidden lg:flex">
+    <div className="pl-2 ">
+      <div>
         <button
           onClick={popular}
           className="mr-3 p-2 lg:mr-8 text-sm lg:text-xl hover:bg-zinc-900 rounded-lg transform transition duration-200 ease-in lg:hover:scale-150 uppercase"
         >
           Popular
         </button>
+      </div>
+      <div>
         <button
           onClick={top_rated}
           className="mr-2 p-2 lg:mr-8 text-sm lg:text-xl hover:bg-zinc-900 rounded-lg transform transition duration-200 ease-in lg:hover:scale-150 uppercase"
         >
           Top rated
         </button>
+      </div>
+      <div>
         <button
           onClick={upcoming}
           className="p-2 text-sm lg:text-xl hover:bg-zinc-900 rounded-lg transform transition duration-200 ease-in lg:hover:scale-150 uppercase"
@@ -62,19 +46,8 @@ const Navbar = ({ setTerm, setUrl, term }) => {
           Upcoming
         </button>
       </div>
-      {/* large NavIcon */}
-
-      {navIcon ? (
-        <div className="absolute right-10 top-14 flex flex-col align-middle justify-center w-32 h-32 bg-zinc-800 text-2xl text-center rounded-md z-50">
-          <NavIcons setTerm={setTerm} setUrl={setUrl} />
-        </div>
-      ) : (
-        ""
-      )}
     </div>
   );
 };
 
-export default Navbar;
-
-// <NavIcons setTerm={setTerm} setUrl={setUrl} />
+export default NavIcons;
